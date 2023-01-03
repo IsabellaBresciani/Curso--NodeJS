8656ef742edb0cc83045b414bee5c2ee858924d1
Isabella Bresciani <isabellabresciani@MacBook-Air-de-Isabella.local>
2023-01-02 22:53:05 -0300

    avances entrega final

--- src/routers/router_products.js
+++ src/routers/router_products.js
@@ -19,4 +19,26 @@ router.post('/', (req, res) => {
     res.send({status: "aproveed", message:"Product added succesfuly!"})
 })
 
+router.put('/', (req,resp)=>{
+    const prod = req.body
+    const idx = products.findIndex(u => u.title.toLowerCase() == prod.title.toLowerCase())
+    if (idx <0){
+        return resp.status(404).json({status: "Error", error: "User not found"})
+    }
+    products[idx]= prod 
+    resp.send({status:'success', message:'Usuario modificado!'})
+})
+
+
+router.delete('/:pid', (req,resp)=>{
+    const id = req.params.pid
+    
+    const length = products.length
+    products = products.filter(u => u.id != id)
+    if(length == products.length){
+        return resp.send({status: "Error", error: "User not found"})
+    }
+    resp.send({status:'success', message:'Usuario eliminado con exito!'})
+})
+
 export default router
